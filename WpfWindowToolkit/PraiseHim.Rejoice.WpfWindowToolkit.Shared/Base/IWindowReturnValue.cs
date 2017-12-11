9574623605b17a6d9454622fb5f8b780b9720079
imnbwd <nbwd@qq.com>
2017-12-11 14:28:11 +0800

    add interfaces to support returning data

---
+++ WpfWindowToolkit/PraiseHim.Rejoice.WpfWindowToolkit.Shared/Base/IWindowReturnValue.cs
@@ -0,0 +1,13 @@
+﻿namespace PraiseHim.Rejoice.WpfWindowToolkit.Base
+{
+    /// <summary>
+    /// Supports returning a value.
+    /// </summary>
+    public interface IWindowReturnValue
+    {
+        /// <summary>
+        /// Get or set the value to be returned.
+        /// </summary>
+        object Value { get; set; }
+    }
+}
