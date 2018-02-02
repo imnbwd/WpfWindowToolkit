12075a0e99258c50e647da53290866adf3eba110
imnbwd <nbwd@qq.com>
2018-02-02 14:19:41 +0800

    update demo

---
+++ WpfWindowToolkit.Samples/WpfWindowToolkit.Demo.Shared/Views/StateControlTestWindow.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfWindowToolkit.Demo.Views
+{
+    /// <summary>
+    /// Interaction logic for StateControlTestWindow.xaml
+    /// </summary>
+    public partial class StateControlTestWindow : Window
+    {
+        public StateControlTestWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
