64c6eeae7ee6ba81d98db1268b667388a69aa6ec
imnbwd <nbwd@qq.com>
2018-02-08 10:36:08 +0800

    update demo

--- WpfWindowToolkit.Samples/WpfWindowToolkit.Demo.Shared/ViewModels/ReturnValueMainWindowViewModel.cs
+++ WpfWindowToolkit.Samples/WpfWindowToolkit.Demo.Shared/ViewModels/ReturnValueMainWindowViewModel.cs
@@ -4,22 +4,36 @@ using WpfWindowToolkit.Demo.Models;
 using WpfWindowToolkit.Demo.Views;
 
 namespace WpfWindowToolkit.Demo.ViewModels
-{   
+{
     public class ReturnValueMainWindowViewModel : ViewModelBaseEx<Friend>
     {
+        public ReturnValueMainWindowViewModel()
+        {
+            ProcessReturnValueCommand = new RelayCommand<Friend>(ProcessReturnValue);
+        }
+
+        public RelayCommand<Friend> ProcessReturnValueCommand { get; set; }
+
         public void ShowFriendSelectionWindow()
         {
-            this.ShowWindow(new OpenWindowInfo { WindowType = typeof(ReturnValueTestWindow) }, friend =>
+            this.ShowWindow(new OpenWindowInfo { WindowType = typeof(ReturnValueTestWindow) }, friend => ShowResult(friend));
+        }
+
+        private void ProcessReturnValue(Friend parameter)
+        {
+            ShowResult(parameter);
+        }
+
+        public void ShowResult(Friend friend)
+        {
+            if (friend != null)
             {
-                if (friend != null)
-                {
-                    MessageBox.Show($"You have selected this friend: {friend.Name}");
-                }
-                else
-                {
-                    MessageBox.Show("No friend has been selected");
-                }
-            });
+                MessageBox.Show($"You have selected this friend: {friend.Name}");
+            }
+            else
+            {
+                MessageBox.Show("No friend has been selected");
+            }
         }
     }
 }
